170b786c8923ace531542c00ccf7eb05a079d1e5
Fabiano de Oliveira Rodrigues <fabiano.1972@hotmail.com>
2018-04-22 00:23:35 -0300

    Adicionado o mecanismo lazy loading para a tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { RankingEstabelecimentoPage } from './../pages/ranking-estabelecimento/ranking-estabelecimento';
 import { BrowserModule } from "@angular/platform-browser";
 import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
@@ -7,25 +6,15 @@ import { StatusBar } from "@ionic-native/status-bar";
 
 import { MyApp } from "./app.component";
 import { Toaster } from "../assets/utils/Toaster";
-import { LoginPage } from "../pages/login/login";
-import { MenuPrincipalPage } from "../pages/menu-principal/menu-principal";
-import { RecuperarSenhaUsuarioPage } from "../pages/recuperar-senha-usuario/recuperar-senha-usuario";
-import { CadastrarUsuarioPage } from "../pages/cadastrar-usuario/cadastrar-usuario";
-import { AlterarSenhaUsuarioPage } from "../pages/alterar-senha-usuario/alterar-senha-usuario";
-import { ConfiguracoesPage } from "../pages/configuracoes/configuracoes";
-import { SobrePage } from "../pages/sobre/sobre";
-import { SolicitarReservaPage } from "../pages/solicitar-reserva/solicitar-reserva";
 
 @NgModule({
   declarations: [
-    MyApp,
-    LoginPage
+    MyApp
   ],
   imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    LoginPage
+    MyApp
   ],
   providers: [
     StatusBar,
