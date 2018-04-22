71260896818eb783cf1b688707c9bb5caa8c78df
Fabiano de Oliveira Rodrigues <fabiano.1972@hotmail.com>
2018-04-22 01:28:50 -0300

    Foi implementada a lógica da tela de Ranking e adicionda a interface Produto

---
+++ src/models/produto.ts
@@ -0,0 +1,8 @@
+export interface Produto{
+    id: number;
+    descricao: string;
+    receita: string;
+    valor: number;
+    foto: string;
+
+}
--- src/pages/ranking-estabelecimento/ranking-estabelecimento.ts
+++ src/pages/ranking-estabelecimento/ranking-estabelecimento.ts
@@ -1,3 +1,4 @@
+import { Produto } from './../../models/produto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -8,7 +9,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RankingEstabelecimentoPage {
 
+  public produtos: Produto[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.produtos = [
+      
+      { id: 1, descricao: "Bolinho de Bacalhau", receita: "Bacalhau kkkk", valor: 6, foto: "assets/imgs/smartQueue_logo.png"},
+      { id: 2, descricao: "Parmegiana", receita: "Sepa tem queijo, molho e um tipo de carne", valor: 23, foto: "assets/imgs/smartQueue_logo.png"},
+      { id: 3, descricao: "Salada Caesar", receita: "Alface fresco diretamente da terra", valor: 16, foto: "assets/imgs/smartQueue_logo.png"},
+      { id: 4, descricao: "Risoto", receita: "Frango birl", valor: 32, foto: "assets/imgs/smartQueue_logo.png"},
+      { id: 5, descricao: "Lanche", receita: "No céu tem pão", valor: 8, foto: "assets/imgs/smartQueue_logo.png"}
+    ];
   }
 
 }
